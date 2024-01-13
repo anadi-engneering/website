9bb3ecc2d6081051d26ba8ec60058c5ba0651914
anadi-engneering <155803188+anadi-engneering@users.noreply.github.com>
2024-01-13 16:38:54 +0000

    Ready website

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+<<<<<<< HEAD
 def home():
+=======
+def index():
+    # The function render_template looks for a file named index.html in the 'templates' folder
+>>>>>>> f49e822 (Ready website)
     return render_template('index.html')
 
 if __name__ == '__main__':
